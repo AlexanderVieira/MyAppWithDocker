75b91edafbb2dc63a49107816ff86e032853b3de
Alexander Vieira da Silva <alexander.silva@al.infnet.edu.br>
2023-08-26 19:01:41 -0300

    Adicionar arquivos de projeto.

---
+++ src/Tests/AVS.Clientes.XUnit/WeatherForecastControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using AVS.Clientes.Api.Controllers;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace AVS.Clientes.XUnit
+{
+
+    public class WeatherForecastControllerTest
+    {
+        [Fact(DisplayName = "Obter temperaturas com sucesso")]
+        [Trait("Categoria", "Weather Forecast Controller Test")]
+        public void WeatherForecastController_Get_DeveExecutarComSucesso()
+        {
+            //Arrange
+            var logger = new Mock<ILogger<WeatherForecastController>>();
+            var ctl = new WeatherForecastController(logger.Object);
+
+            //Act
+            var result = ctl.Get();
+
+            //Asset
+            result.Should().HaveCountGreaterThan(0);
+        }
+    }
+}
